c01b79cab90f698107605aff1cdfd5d75573ebf1
Mau <maura2403@gmail.com>
2020-08-04 17:50:00 -0300

    entre 768px y 949 hay problemas con el layout, el boton va mejorando

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+menuButton.addEventListener('click', () => {
+    if (menu.className != 'activeMenu menu') {menu.className = 'activeMenu menu';  }
+    else {menu.className = 'menu';}
 
+});
